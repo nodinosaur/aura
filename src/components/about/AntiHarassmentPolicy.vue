433b116119b07b8620dd22997857a3b0009d89cd
George Medve <george.medve@gmail.com>
2019-12-30 19:06:22 +0000

    American spelling fixes and a typo

--- src/components/about/AntiHarassmentPolicy.vue
+++ src/components/about/AntiHarassmentPolicy.vue
@@ -29,10 +29,10 @@
                         <li class="google-font">Nationality</li>
                     </ul>
 
-                    <span class="google-font" style="font-size:110%">The above is not an exhaustive list -- we do not tolerate harassment of event spanarticipants in any form. </span>
+                    <span class="google-font" style="font-size:110%">The above is not an exhaustive list -- we do not tolerate harassment of event participants in any form.</span>
                 </p>
 
-                <p class="google-font" style="font-size:120%">Sexual language and imagery is not appropriate for any event venue, including talks. Event participants violating these rules may be expelled from the event, and event banned from future events at the discretion of the event organizers/management. </p>
+                <p class="google-font" style="font-size:120%">Sexual language and imagery is not appropriate for any event venue, including talks. Event participants violating these rules may be expelled from the event, and event banned from future events at the discretion of the event organisers/management. </p>
 
                 <p class="google-font" style="font-size:110%">Harassment includes (but is not limited to):
                     <ul class="mt-2" style="font-size:110%">
@@ -49,9 +49,9 @@
 
                 <p class="google-font" style="font-size:120%">Participants asked to stop any harassing behavior are expected to comply immediately. 
                     <br><br>
-                    Exhibiting partners and guests are also subject to the anti-harassment policy. In particular, exhibitors and speakers should not use sexualized images, activities, or other material, or otherwise create a sexualized environment in their slide decks, exhibit material, exhibit staffing, promotional items or demo material. 
+                    Exhibiting partners and guests are also subject to the anti-harassment policy. In particular, exhibitors and speakers should not use sexualised images, activities, or other material, or otherwise create a sexualised environment in their slide decks, exhibit material, exhibit staffing, promotional items or demo material. 
                     <br><br>
-                    If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact an organizer or event volunteer immediately. Organizers and event volunteers may be identified by t-shirts or special badges/lanyards. Organizers will investigate the issue and take appropriate action. This may include helping participants contact venue security or local law enforcement, provide escorts, or otherwise assist these experiencing harassment to fell safe for the duration of the event. 
+                    If you are being harassed, notice that someone else is being harassed, or have any other concerns, please contact an organizer or event volunteer immediately. Organisers and event volunteers may be identified by t-shirts or special badges/lanyards. Organisers will investigate the issue and take appropriate action. This may include helping participants contact venue security or local law enforcement, provide escorts, or otherwise assist these experiencing harassment to fell safe for the duration of the event. 
                     <br><br>
                     Though we hope that we never have to invoke this policy, we believe that having this document helps everyone think a little more about how their actions and words affect the whole community, as well as individuals in the community.
                 </p>
