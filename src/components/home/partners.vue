727864fcd0fc31bf35fbef2e192cd840de847e76
George Medve <george.medve@gmail.com>
2019-12-31 02:28:08 +0000

    added a feature event

--- src/components/home/partners.vue
+++ src/components/home/partners.vue
@@ -4,7 +4,7 @@
             <v-flex xs12 md12>
                 <p class="google-font mt-2 mb-0" style="font-size:170%;color:#37474F;" >Partners</p>
                 <p class="google-font mt-1 mb-0" style="font-size:120%">A very big thank you to all our partners for their continued partnership.</p>
-                <p class="google-font mt-0" style="font-size:120%">If you’re interested in being showcased throughout <b>{{chapterDetails.ChapterName}}</b>, contact <a style="color:#1565C0;text-decoration: none;" :href="`mailto:${chapterDetails.ChapterEmail}`">{{chapterDetails.ChapterEmail}}</a> to discuss sponsorship opportunities.</p>
+                <p class="google-font mt-0" style="font-size:120%">If you’re interested in being showcased throughout <b>{{chapterDetails.ChapterName}}</b>, contact <a style="color:#1565C0;text-decoration: none;" :href="`mailto:${chapterDetails.ChapterSponsorEmail}`">{{chapterDetails.ChapterSponsorEmail}}</a> to discuss sponsorship opportunities.</p>
             </v-flex>
 
             <v-flex xs12 md12 class="mt-2">
